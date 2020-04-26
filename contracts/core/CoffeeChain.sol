ce3d639a3c7f983e680638277145884a210a469a
emanherawy <eman.herawy@gmail.com>
2020-04-26 10:51:03 +0200

    init

---
+++ contracts/core/CoffeeChain.sol
@@ -0,0 +1,29 @@
+// pragma solidity ^0.5.10;
+// import './Ownable.sol';
+// import '../base/SupplyChain.sol';
+ 
+
+
+
+// contract CoffeeChain is Ownable , SupplyChain{
+
+// constructor(address _farmerAddress  ,address _consumerAddress , address _retailerAddress  , address _distributorAddress) public payable
+//      SupplyChain( _farmerAddress  , _consumerAddress ,  _retailerAddress  ,  _distributorAddress){
+
+// }
+
+//   /**overid access controle functions  */
+//       function setFarmerAddress(address contractAddress) public onlyOwner()  returns (bool status) {
+//             super._setFarmerAddress(contractAddress);
+//       }
+//     function setConsumerAddress(address contractAddress) public onlyOwner()  returns (bool status) {
+//             super._setConsumerAddress(contractAddress);
+//       }
+//     function setRetailerAddress(address contractAddress) public onlyOwner()  returns (bool status) {
+//            super._setRetailerAddress(contractAddress);
+//       }
+//     function setDistributorAddress(address contractAddress) public onlyOwner() returns (bool status) {
+//             super._setDistributorAddress(contractAddress);
+//       }
+
+// }
